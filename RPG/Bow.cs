0b1b2982d79f9aff808bedec115c8bf84d9db23a
David Ccolcca <draigner143@gmail.com>
2025-09-08 21:59:54 -0500

    Create Bow.cs

---
+++ RPG/Bow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    public class Bow : Weapon
+    {
+        public Bow(int damage = 12) : base("Arco", damage) { }
+        public override void Use(Character target)
+        {
+            Console.WriteLine("Disparas una flecha certera!");
+            target.TakeDamage(damage);
+        }
+    }
+}
