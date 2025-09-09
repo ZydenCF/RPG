15139cc749c3cba46658ddfe740dcf5c7e20a672
David Ccolcca <draigner143@gmail.com>
2025-09-08 22:02:52 -0500

    Create StageData.cs

---
+++ RPG/StageData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    public struct StageData
+    {
+        public int stageNumber;
+        public List<Enemy> enemies;
+        public string description;
+        public StageData(int number, List<Enemy> enemies, string description)
+        {
+            this.stageNumber = number;
+            this.enemies = enemies;
+            this.description = description;
+        }
+    }
+}
