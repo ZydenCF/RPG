81b47729c608e4bd9c59b5ad9598f9658d865c6b
David Ccolcca <draigner143@gmail.com>
2025-09-08 21:56:32 -0500

    Create Weapon.cs

---
+++ RPG/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    public abstract class Weapon : IUsable
+    {
+        protected int damage;
+        protected string weaponName;
+        public Weapon(string name, int damage) { this.weaponName = name; this.damage = damage; }
+        public string GetName() { return weaponName; }
+        public abstract void Use(Character target);
+    }
+}
